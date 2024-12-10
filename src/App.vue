fc687e8924093fdc535c13c235a6b65b66cc7517
Dario Ferreyra <dferreyra@infracommerce.lat>
2024-12-10 03:36:48 -0300

    Added Contador component

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Contador />
+</template>
+
+<script setup lang="ts">
+import Contador from './components/Contador.vue'
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
---
+++ src/components/Contador.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>{{ contador }}</p>
+  <div>
+    <button @click="incrementar">Incrementar</button>
+    <button @click="disminuir">Disminuir</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const contador = ref(0)
+
+const incrementar = () => {
+  contador.value++
+}
+
+const disminuir = () => {
+  if (contador.value > 0) contador.value--
+}
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
